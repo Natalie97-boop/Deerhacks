2329d6f29fb6afc3aab7d5ad89598334460f36bc
dqdotz <davidq.z.zhao@gmail.com>
2022-05-01 11:49:22 -0400

    added logging of past generated jokes to logs

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 pattern = '^[Hh][Ee][Yy] [Dd][Aa][Dd],?.*'
-words = []
+words = [] # for future key word updates
 
 description = ''
 
@@ -20,12 +20,28 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    server_name = message.guild.name
+    user_name = message.author
 
-    if message.author == client.user:
+    if user_name == client.user:
+        return
+
+    elif re.search('^[Mm][Oo]([Mm]|[Tt][Hh][Ee][Rr])', user_name):
+        await message.channel.send('Nothing, dear.')
         return
 
     if re.search(pattern, message.content):
+        # TODO forward pass to generate joke here
         msg = generate_joke()
+        try:
+            with open("logs/{}.txt".format(server_name), "a+") as myfile:
+                myfile.write("{}: {}".format(user_name, msg))
+            
+        except:
+            with open("logs/{}.txt".format(server_name), "w") as myfile:
+                myfile.write("{}: {}".format(user_name, msg))
+
         await message.channel.send(msg)
+        return
             
 client.run(TOKEN)
